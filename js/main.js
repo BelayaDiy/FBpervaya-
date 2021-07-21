20befbe12c46ee81d5605f81f20c3e9a6b5e1d77
BelayaDiy <87760961+BelayaDiy@users.noreply.github.com>
2021-07-21 14:43:28 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$('.openmodal').click(function (e) {
+    e.preventDefault();
+    $('.kadobagud').addClass('midsalod');
+    });
+  $('.closemodal').click(function (e) {
+    e.preventDefault();
+    $('.kadobagud').removeClass('midsalod');
+    });
